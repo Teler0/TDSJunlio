645ff86bc64ba59b58f9435afe9ca19533690e44
albertogarciasz <albertoaljada@gmail.com>
2020-05-07 13:07:13 +0200

    dd

---
+++ AppChat/maven-lab.bat
@@ -0,0 +1,5 @@
+SET M2_HOME=C:\users\alumno\tds\apache-maven-3.6.2
+SET M2=C:\users\alumno\tds\apache-maven-3.6.2\bin
+SET PATH=%M2%;%JAVA_HOME%\bin;%PATH%
+SET JAVA_HOME=C:\Archivos de programa\Java\jdk1.8.0_221
+SET M2_REPO=C:\users\alumno\tds\.m2\repository_local
